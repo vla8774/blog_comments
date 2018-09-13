5ade2a2ecb5046bffb5dd1cc0e0cc9f5e20e63d2
Кузнецов Владимир Вячеславович <F0621037@RG62.LOCAL>
2018-09-13 13:48:18 +0300

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\F0621037.RG62\PycharmProjects\blog_comments\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
